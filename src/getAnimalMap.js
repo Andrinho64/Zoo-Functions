cc70ac7e8ff2caefdb3689241676b0a1c8ca04bf
Andrew Caetano <andrewr_caetano@hotmail.com>
2023-12-04 10:19:33 -0300

    [Andrew Caetano] Zoo Functions correcoes4

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
-const getAnimalMap = (options) => {
-  // seu código aqui
+const mapResidents = (residents, sex, sorted) => {
+  const filteredResidents = residents.filter((resident) => (!sex || resident.sex === sex));
+  const names = filteredResidents.map((resident) => resident.name);
+  return sorted ? names.sort() : names;
+};
+
+const processSpecies = (acc, species, includeNames, sorted, sex) => {
+  const { location, name, residents } = species;
+
+  if (!acc[location]) {
+    acc[location] = [];
+  }
+
+  if (includeNames) {
+    acc[location].push({ [name]: mapResidents(residents, sex, sorted) });
+  } else {
+    acc[location].push(name);
+  }
+
+  return acc;
+};
+
+const getAnimalMap = (options = {}) => {
+  const { includeNames, sorted, sex } = options;
+
+  return data.species.reduce((acc, species) =>
+    processSpecies(acc, species, includeNames, sorted, sex), {});
 };
 
 module.exports = getAnimalMap;
